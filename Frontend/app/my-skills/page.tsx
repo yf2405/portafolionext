fb505faaf2928cbf741fcbef49706860c8d863d1
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-04-25 12:26:06 -0500

    correccion letras

--- Frontend/app/my-skills/page.tsx
+++ Frontend/app/my-skills/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
       <div className="flex flex-col gap-20 max-w-[80%] text-center items-center">
         <div className="flex flex-col items-center gap-4">
           <h1 className="font-semibold text-white text-[50px]">
-            Abilidades{" "}
+          Habilidades{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               &{" "}
@@ -23,7 +23,7 @@ const Page = () => {
             Technologies
           </h1>
           <p className="text-gray-400 text-[20px]">
-          últimas tecnologías
+          últimas Tecnologías
           </p>
         </div>
         <Swiper
