9eaab59f37f9845507a8f0992d7061a10193ddcd
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-05-24 12:43:05 -0500

    correccion text title

--- Frontend/constants/index.ts
+++ Frontend/constants/index.ts
@@ -107,7 +107,7 @@ export const Projects = [
   },
   {
     title: "E-commerce-Xcosas",
-    text: "Api que trae gran unformacion de pokemones en forma de cartas, mostrando sus evoluciones. Creado con Javascript.",
+    text: "La página web representa una solución integral para el comercio electrónico de productos exclusivos y difíciles de encontrar en Colombia. Login y Crud, solo para administrador.",
     src: "/Xcosas-Project.png",
     git: "https://github.com/yf2405/api-pokemon",
     Link: "https://xcosas.up.railway.app/home",
